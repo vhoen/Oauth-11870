3676fdc906461b46afc1da1fcc1f76098d1d0576
vhoen <vincent.hoen@gmail.com>
2016-02-08 16:43:56 +0000

    further tests to no avail

--- oauth.php
+++ oauth.php
@@ -1,125 +1,147 @@
+<pre>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
+$conf = include 'conf.php';
 
-$appToken = 'APP_TOKEN';
-$appSecret = 'APP_SECRET';
+$appToken = $conf['appToken'];
+$appSecret = $conf['appSecret'];
 
 //first step
 //let's get an unauthorized request token
+
 if (count($_REQUEST) == 0) {
     $url = 'https://11870.com/services/manage-api/request-token';
-    
     $params = array(
-        'oauth_consumer_key' => rawurlencode($appToken) ,
-        'oauth_signature_method' => rawurlencode('HMAC-SHA1') ,
-        'oauth_timestamp' => rawurlencode(time()) ,
-        'oauth_nonce' => rawurlencode(uniqid()) ,
-        'oauth_version' => rawurlencode('1.0') ,
-        'oauth_callback' => 'http://localhost/oauth.php',
+        'oauth_consumer_key' => $appToken,
+        'oauth_signature_method' => 'HMAC-SHA1',
+        'oauth_timestamp' => time(),
+        'oauth_nonce' => uniqid(),
+        'oauth_version' => '1.0',
+        'oauth_callback' => sprintf('%s://%s%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['SERVER_NAME'], $_SERVER['REQUEST_URI']),
     );
-    
+
     //creating signing key with appSecret and nothing else since there is no token at this point
-    $signingKey = $appSecret . '&';
-    
+    $signingKey = rawurlencode($appSecret) . '&';
+
     $output = getOutput($url, $params, $signingKey);
-    
+
     parse_str($output, $values);
-    
+
     if (isset($values['oauth_token']) && isset($values['oauth_token_secret'])) {
         $authToken = $values['oauth_token'];
         $authTokenSecret = $values['oauth_token_secret'];
-        
+
         //token secret is stored to be used later
         $_SESSION['oauth_token_secret'] = $values['oauth_token_secret'];
-        
+
         $url = 'https://11870.com/services/manage-api/authorize';
-        
+
         $authorizeUrl = $url . '?oauth_token=' . $values['oauth_token'];
-        
+
         header('Location: ' . $authorizeUrl);
-    }
-    else{
+    } else {
         var_dump($values);
     }
-} 
-else if (isset($_REQUEST['oauth_token']) && isset($_REQUEST['oauth_verifier'])) {
+} else if (isset($_REQUEST['oauth_token']) && isset($_REQUEST['oauth_verifier'])) {
     $oauthToken = $_REQUEST['oauth_token'];
     $oauthVerifier = $_REQUEST['oauth_verifier'];
-    
+
     $url = 'https://11870.com/services/manage-api/access-token';
-    
+
     $params = array(
-        'oauth_consumer_key' => rawurlencode($appToken) ,
+        'oauth_consumer_key' => rawurlencode($appToken),
         'oauth_token' => $oauthToken,
-        'oauth_signature_method' => rawurlencode('HMAC-SHA1') ,
-        'oauth_timestamp' => rawurlencode(time()) ,
-        'oauth_nonce' => rawurlencode(uniqid()) ,
-        'oauth_version' => rawurlencode('1.0') ,
+        'oauth_signature_method' => rawurlencode('HMAC-SHA1'),
+        'oauth_timestamp' => rawurlencode(time()),
+        'oauth_nonce' => rawurlencode(uniqid()),
+        'oauth_version' => rawurlencode('1.0'),
         'oauth_verifier' => $oauthVerifier,
     );
-    
+
     $signingKey = $appSecret . '&' . $_SESSION['oauth_token_secret'];
-    
+
     $output = getOutput($url, $params, $signingKey);
-    
+
     parse_str($output, $values);
-    
+
     if (isset($values['oauth_token']) && isset($values['oauth_token_secret'])) {
         $oauthToken = $values['oauth_token'];
         $oauthTokenSecret = $values['oauth_token_secret'];
-        
+
         echo sprintf('Auth success : token : %s', $oauthToken);
-    } 
-    else {
+    } else {
         echo 'Auth failed';
     }
 }
 
-function getOauthQueryString($params, $separator = '&') {
-    $queryStringPairs = array();
-    foreach ($params as $kParams => $vParams) {
-        $queryStringPairs[] = $kParams . '=' . rawurlencode($vParams);
-    }
-    
-    return implode($separator, $queryStringPairs);
+function buildSignatureBaseString($method, $url, $params)
+{
+    $urlElements = parse_url($url);
+
+    $signatureParams = array(
+        rawurlencode(strtoupper($method)),
+        rawurlencode(sprintf('%s://%s%s', $urlElements['scheme'], $urlElements['host'], $urlElements['path'])),
+        rawurlencode(getOauthQueryString($params)),
+    );
+
+    return implode('&', $signatureParams);
 }
 
-function getOutput($url, $params, $signingKey) {
-    
-    //sorting params by alphabetical order
+function buildSignature($signingKey, $signatureBaseString)
+{
+    $hash = hash_hmac('sha1', $signatureBaseString, $signingKey, true);
+
+    return base64_encode($hash);
+}
+
+function getOauthQueryString($params, $separator = '&')
+{
+    $pairs = array();
+    foreach ($params as $kParams => $vParams) {
+        $k = rawurlencode(utf8_encode($kParams));
+        $v = rawurlencode(utf8_encode($vParams));
+
+        $pairs[$k] = $k . '=' . $v;
+    }
+
+    return implode('&', $pairs);
+}
+
+function getOutput($url, $params, $signingKey)
+{
+
+    $urlElems = parse_url($url);
+
     ksort($params);
-    
-    //gathering signature params
-    $signatureParams = array(
-        'GET',
-        rawurlencode($url) ,
-        rawurlencode(getOauthQueryString($params)) ,
-    );
-    
+
     //creating signature params
-    $signature = implode('&', $signatureParams);
-    
-    //creating signature
-    $signedSignature = base64_encode(hash_hmac('sha1', $signature, $signingKey, true));
-    
+    $signatureBaseString = buildSignatureBaseString('GET', $url, $params);
+
+    $signature = buildSignature($signingKey, $signatureBaseString);
+    var_dump($url, $params, $signingKey, $signatureBaseString, $signature);
+
     //and adding it to params
-    $params['oauth_signature'] = $signedSignature;
-    
+    $params['oauth_signature'] = $signature;
+
     $fullUrl = $url . '?' . getOauthQueryString($params);
-    
+
+    $headers = 'Authorization: OAuth realm=""';
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $fullUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    
-    // curl_setopt($ch, CURLOPT_VERBOSE, true);
-    // curl_setopt($ch, CURLOPT_STDERR, fopen('php://output', 'w'));
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: OAuth realm="' . $url . '"'));
-    
+
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_setopt($ch, CURLOPT_STDERR, fopen('php://output', 'w'));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/x-www-form-urlencoded',
+        'OAuth realm=""',
+    ));
+
     $output = curl_exec($ch);
     curl_close($ch);
-    
+
     return $output;
 }
